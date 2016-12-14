b43173a6022bf7a46a14a8d86909e81711efaf79
Alison Campbell <alisonc123@gmail.com>
2016-12-14 18:51:11 +0000

    Add seller page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
-Rails.application.routes.draw do
+Etsydemo::Application.routes.draw do
   devise_for :users
   resources :listings
-  get 'pages/about'
 
-  get 'pages/contact'
+  get "pages/about"
+  get "pages/contact"
+  get 'seller' => "listings#seller"
 
-  root "listings#index"
+  root 'listings#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
